7c0edc48b201389a6852f84375643247dc541215
AhmadRadhawaOfficial <official.ahmadrandhawa@gmail.com>
2024-12-25 20:26:07 +0500

    Add web_server

--- Web_browser.py
+++ Web_browser.py
@@ -1,6 +1,6 @@
 import socket   # The socket is like a phone, it allows your comp to talk to other comp over the internet
 
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Used IPv4 & TCP/IP for communication.
+mysock = socket.socket(AF_INET, SOCK_STREAM)  # Used IPv4 & TCP/IP for communication.
 mysock.connect(('data.pr4e.org', 80))
 cmd = 'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
