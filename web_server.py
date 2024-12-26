e1f6580397c88f88854c99871a1609fa875474ef
Ahmad Hussain <official.ahmadrandhawa@gmail.com>
2024-12-26 08:23:52 +0500

    Update web_server.py

--- web_server.py
+++ web_server.py
@@ -3,27 +3,16 @@ from socket import *
 def createServer():
     server_socket = socket(AF_INET, SOCK_STREAM)
     try: 
-<<<<<<< HEAD
         server_socket.bind(('localhost', 9000))  # My computer, port number.
         server_socket.listen(5)  # if server busy, max 5 requests are placed in a backlog queue.
         while(1):
             (client_socket, address) = server_socket.accept()   # Wait for client to connect.
-=======
-        server_socket.bind(('localhost', 9000))
-        server_socket.listen(5)
-        while(1):
-            (client_socket, address) = server_socket.accept()
->>>>>>> 7c0edc48b201389a6852f84375643247dc541215
 
             rd = client_socket.recv(5000).decode()
             pieces = rd.split("\n")
             if len(pieces) > 0: print(pieces[0])
 
-<<<<<<< HEAD
             data = "HTTP/1.1 200 OK\r\n"
-=======
-            data = "HTTP/1.1 PK\r\n"
->>>>>>> 7c0edc48b201389a6852f84375643247dc541215
             data += "Content-Type: text/html; charset=utf-8\r\n"
             data += "\r\n"
             data += "<html><body>Hello World</body></html>\r\n\r\n"
@@ -41,6 +30,3 @@ def createServer():
 print("Access http://localhost:9000")
 
 createServer()
-
-
-            
